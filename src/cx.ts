3b3e80006e82acc8f00702348922ad6ac61b4241
Tim Phan <timphan.dev@gmail.com>
2024-07-28 22:26:00 +0700

    chore: improve perf

--- src/cx.ts
+++ src/cx.ts
@@ -2,31 +2,33 @@ type Arg = string | number | null | undefined | Record<string, boolean>
 
 export const cx = (...args: (Arg | Arg[])[]) => {
   let className = ''
+  let i = 0
+  let arg: Arg | Arg[]
+  let next: string
 
-  for (const arg of args) {
-    if (typeof arg === 'string') {
-      className += (className ? ' ' : '') + arg.trim()
-      continue
-    }
-
-    if (typeof arg === 'number') {
-      className += (className ? ' ' : '') + arg
-      continue
-    }
-
-    if (arg && typeof arg === 'object') {
-      if (Array.isArray(arg)) {
-        className += (className ? ' ' : '') + cx(...arg)
-        continue
-      }
-
-      for (const key in arg) {
-        if (arg[key]) {
-          className += (className ? ' ' : '') + key.trim()
+  for (i = 0; i < args.length; i++) {
+    if ((arg = args[i])) {
+      if (typeof arg === 'string') {
+        className && (className += ' ')
+        className += arg.trim()
+      } else if (typeof arg === 'number') {
+        className && (className += ' ')
+        className += arg
+      } else if (typeof arg === 'object') {
+        if (Array.isArray(arg)) {
+          if ((next = cx(...arg))) {
+            className && (className += ' ')
+            className += next
+          }
+        } else {
+          for (const key in arg) {
+            if (arg[key]) {
+              className && (className += ' ')
+              className += key
+            }
+          }
         }
       }
-
-      continue
     }
   }
 
